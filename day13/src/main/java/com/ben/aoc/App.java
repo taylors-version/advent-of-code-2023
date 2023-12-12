0e3af73c435ff1981da27851efdc422654faa0f4
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2023-12-12 14:56:11 +0000

    Day 13 setup

---
+++ day13/src/main/java/com/ben/aoc/App.java
@@ -0,0 +1,12 @@
+package com.ben.aoc;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Oasis oasis = new Oasis();
+        System.out.println("day13 puzzle1: " + oasis.findNextValues("input.txt"));
+        
+    }
+}
