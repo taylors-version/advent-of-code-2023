5e64701384d498ea8553712d1be7f4bd5ea6cff2
Ben Healy <benjamin.p.healy@gmail.com>
2024-01-08 11:38:44 +0000

    Extra utilities for 2019 AoC

--- utilities/src/main/java/com/ben/aoc/maths/Maths.java
+++ utilities/src/main/java/com/ben/aoc/maths/Maths.java
@@ -1,5 +1,6 @@
 package com.ben.aoc.maths;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Maths {
@@ -52,5 +53,34 @@ public class Maths {
 
 		return gcd((b - a) >> 1, a);
 	}
+	
+	public static <T> List<T[]> getAllPermutations(int n, T[] elements, List<T[]> permutations){
+		
+		
+        if(n == 1) {
+        	T[] copy = Arrays.copyOf(elements, elements.length);
+            permutations.add(copy);
+        } else {
+            for(int i = 0; i < n-1; i++) {
+            	getAllPermutations(n - 1, elements, permutations);
+                if(n % 2 == 0) {
+                    swap(elements, i, n-1);
+                } else {
+                    swap(elements, 0, n-1);
+                }
+            }
+            getAllPermutations(n - 1, elements, permutations);
+        }
+
+		
+		return permutations;
+	}
+	
+    private static <T> void swap(T[] elements, int a, int b) {
+
+        T tmp = elements[a];
+        elements[a] = elements[b];
+        elements[b] = tmp;
+    }
 
 }
